55fdacb4673f2e2727fde585ebabbb5adc9c6671
Schuyler Smith <sdsmith@iastate.edu>
2020-02-11 12:09:24 -0600

    Update fastq_to_fasta.sh

--- fastq_to_fasta.sh
+++ fastq_to_fasta.sh
@@ -3,4 +3,4 @@ awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' read
 
 -or-
 
-sed -n '1~4s/^@/>/p;2~4p' in.fastq > out.fasta
+for file in $(ls fastq); do file_name="$(basename -- $file)"; file_name=$(echo "$file_name" | cut -f 1 -d '.'); sed -n '1~4s/^@/>/p;2~4p' fastq/${file_name}*.fastq > fasta/${file_name}.fasta; done
